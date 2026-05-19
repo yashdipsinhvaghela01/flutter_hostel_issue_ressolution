8b01d65b4f73638fa5221a95ec5b3afb3dc459dd
Yashdipsinh Vaghela <yashdipsinhvaghela4@gmail.com>
2026-05-19 15:28:05 +0530

    added fragment

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'widgets/Splash.dart';
+import 'widgets/fragment_holder.dart';
 void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
-        body:Splash() ,
+        body:FragmentHolder(),
       ),
     ),
   );
